904c4fdcaba5e680a1558932ee912c7bfbfa7ee2
cwhuang29 <hq678835@gmail.com>
2023-03-31 14:54:36 -0700

    Update axios setting

--- src/shared/services/roots.js
+++ src/shared/services/roots.js
@@ -1,25 +1,31 @@
-import axios from 'axios';
-import config from 'config.js';
+// eslint-disable-next-line import/no-unresolved
+import { BACKEND_URL } from '@env';
 
-// https://axios-http.com/docs/req_config
+import { STORAGE_LOGIN_INFO } from 'shared/constants/storage';
+import logger from 'shared/utils/logger';
+
+import axios from 'axios';
+
 const httpConfig = {
-  baseURL: config.baseURL,
+  baseURL: BACKEND_URL,
   withCredentials: true, // Indicates whether or not cross-site Access-Control requests should be made using credentials
   xsrfHeaderName: 'X-CSRF-Token', // the name of the http header that carries the xsrf token value
   xsrfCookieName: 'csrftoken', // The name of the cookie to use as a value for xsrf token
   timeout: 30000, // If the request takes longer than `timeout`, the request will be aborted (Error: timeout of 1000ms exceeded)
-  headers: { 'X-Questionnaire-Header': 'ntnu' }, // Custom headers to be sent
-  validateStatus: status => status >= 200 && status <= 302, // Defines whether to resolve or reject the promise for a given response
+  headers: { 'X-VH-Source': 'mobile' }, // Custom headers to be sent
   transformResponse: [data => ({ ...JSON.parse(data) /* , timeStamp: new Date() */ })], // Changes to the response to be made before it is passed to then/catch
 };
 
-// const token = document.head.querySelector('meta[name="csrf-token"]')?.content;
-// if(token) {
-//   axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
-// }
-
 const fetch = axios.create(httpConfig);
 
-// fetch.interceptors.request.use(function () {/*...*/});
+const beforeReqIsSent = async config => {
+  const newConfig = config;
+  const loginData = await storage.getData(STORAGE_LOGIN_INFO);
+
+  newConfig.headers['X-VH-Auth'] = loginData || '';
+  logger(`Request is about to send. Config: ${newConfig}`);
+  return newConfig;
+};
+fetch.interceptors.request.use(beforeReqIsSent);
 
 export default fetch;
