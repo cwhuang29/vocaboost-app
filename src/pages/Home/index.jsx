370ef027154a7eebd63eef3dc4ed8d31b6aed287
yunchipang <yunchipang@gmail.com>
2023-04-02 13:08:03 -0700

    Revise Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,64 +2,61 @@ import React, { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
 import PropTypes from 'prop-types';
-import { VStack, Center, Flex } from 'native-base';
+
+import { Center, Flex, VStack } from 'native-base';
 
 import { STORAGE_LOGIN_INFO } from 'shared/constants/storage';
 import storage from 'shared/storage';
 import logger from 'shared/utils/logger';
 
-
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 const Home = () => {
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfoCache, setUserInfoCache] = useState([]);
 
-    const [loggedIn, setloggedIn] = useState(false);
-    const [userInfoCache, setUserInfoCache] = useState([]);
+  const handleClick = () => {
+    logger('Center component clicked');
+  };
 
-    const handleClick = () => {
-        logger('Center component clicked');
-        // console.log('Center component clicked');
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const isSignedIn = await GoogleSignin.isSignedIn();
+      setloggedIn(isSignedIn);
+      if (isSignedIn) {
+        const loginData = await storage.getData(STORAGE_LOGIN_INFO);
+        setUserInfoCache(loginData);
+      } else {
+        setUserInfoCache([]);
+      }
     };
+    getUserInfo();
+  }, []);
 
-    useEffect(() => {
-        const getUserInfo = async () => {
-        const isSignedIn = await GoogleSignin.isSignedIn();
-        setloggedIn(isSignedIn);
-        if (isSignedIn) {
-            const loginData = await storage.getData(STORAGE_LOGIN_INFO);
-            setUserInfoCache(loginData);
-        } else {
-            setUserInfoCache([]);
-        }
-        };
-        getUserInfo();
-    }, []);
+  // eslint-disable-next-line react/no-unstable-nested-components
+  const HomeBox = ({ text, handleClick, bg }) => (
+    <TouchableOpacity onPress={handleClick}>
+      <Center w='64' h='64' bg={bg} rounded='md' shadow={3} _text={{ color: 'white' }}>
+        {text}
+      </Center>
+    </TouchableOpacity>
+  );
 
-    return (
-        <Flex flex={1} justifyContent="center">
-            <VStack space={4} alignItems="center">
-                <TouchableOpacity onPress={handleClick}>
-                    <Center w="64" h="64" bg="indigo.300" rounded="md" shadow={3} _text={{color: "white"}}>GRE 1500</Center>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={handleClick}>
-                    <Center w="64" h="64" bg="indigo.500" rounded="md" shadow={3} _text={{color: "white"}}>Collected Words</Center>
-                </TouchableOpacity>
-            </VStack>
-        </Flex>
-    );
+  return (
+    <Flex flex={1} justifyContent='center'>
+      <VStack space={4} alignItems='center'>
+        <HomeBox text='GRE 1500' handleClick={handleClick} bg='indigo.300' />
+        <HomeBox text='Collected Words' handleClick={handleClick} bg='indigo.600' />
+      </VStack>
+    </Flex>
+  );
 };
-
-// Home.propTypes = {
-//   navigation: PropTypes.object.isRequired,
-//   route: PropTypes.object.isRequired,
-// };
 
 export default Home;
