5789bd37657a5bf0a8c0787335aa7cb06f32d247
yunchipang <yunchipang@gmail.com>
2023-04-03 15:02:35 -0700

    Add param to Home->Study navigation, enable random order of GRE words

--- src/pages/StudyScreen/index.jsx
+++ src/pages/StudyScreen/index.jsx
@@ -1,43 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import PropTypes from 'prop-types';
 
-import { STORAGE_LOGIN_INFO } from 'shared/constants/storage';
-import storage from 'shared/storage';
 import { DEFAULT_CONFIG } from 'shared/utils/config';
 import logger from 'shared/utils/logger';
 import { genWordDetailMap } from 'shared/utils/word';
 
 
 const StudyScreen = ({ navigation, route }) => {
-  const [loggedIn, setloggedIn] = useState(false);
-  const [userInfoCache, setUserInfoCache] = useState([]);
+  const { wordListType } = route.params;
   const [currentIndex, setCurrentIndex] = useState(1);
   const [config, setConfig] = useState(DEFAULT_CONFIG);
   const lang = config.language;
 
-  useEffect(() => {
-    const getUserInfo = async () => {
-      const isSignedIn = await GoogleSignin.isSignedIn();
-      setloggedIn(isSignedIn);
-      if (isSignedIn) {
-        const loginData = await storage.getData(STORAGE_LOGIN_INFO);
-        setUserInfoCache(loginData);
-      } else {
-        setUserInfoCache([]);
-      }
-    };
-    getUserInfo();
-  }, []);
-
   const onPress = () => {
-    if (currentIndex < wordsMap.size) {
-      setCurrentIndex(currentIndex + 1);
-    }
+    nextIndex = Math.floor(Math.random() * wordsMap.size) + 1;
+    setCurrentIndex(nextIndex);
   }
 
-  const wordsMap = genWordDetailMap();
+  const wordsMap = genWordDetailMap(route.params['wordListType']);
   const currentWord = wordsMap.get(currentIndex);
   const { example, meaning, partsOfSpeech } = currentWord.detail[0];
 
