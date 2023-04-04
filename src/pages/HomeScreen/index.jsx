ede93ebadf6be62aaae9165e33b2c4a6e4d99543
cwhuang29 <hq678835@gmail.com>
2023-04-04 01:04:26 -0700

    Connect login/logout with backend server, and setup JWT token mechanism for entire project

--- src/pages/HomeScreen/index.jsx
+++ src/pages/HomeScreen/index.jsx
@@ -5,26 +5,10 @@ import PropTypes from 'prop-types';
 
 import { Center, Flex, VStack } from 'native-base';
 
-<<<<<<< HEAD
 import { STORAGE_USER } from 'shared/constants/storage';
 import storage from 'shared/storage';
 import logger from 'shared/utils/logger';
 
-=======
-import { STORAGE_LOGIN_INFO } from 'shared/constants/storage';
-import storage from 'shared/storage';
-import logger from 'shared/utils/logger';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
->>>>>>> 64fde0a (Fix navigation bug, move ColorPalete component, and revise HomeScreen)
 const HomeBox = ({ text, bg, onPress }) => (
   <TouchableOpacity onPress={onPress}>
     <Center w='64' h='64' bg={bg} rounded='md' shadow={3} _text={{ color: 'white' }}>
@@ -42,11 +26,7 @@ const HomeScreen = ({ navigation }) => {
       const isSignedIn = await GoogleSignin.isSignedIn();
       setloggedIn(isSignedIn);
       if (isSignedIn) {
-<<<<<<< HEAD
         const loginData = await storage.getData(STORAGE_USER);
-=======
-        const loginData = await storage.getData(STORAGE_LOGIN_INFO);
->>>>>>> 64fde0a (Fix navigation bug, move ColorPalete component, and revise HomeScreen)
         setUserInfoCache(loginData);
       } else {
         setUserInfoCache([]);
@@ -57,15 +37,15 @@ const HomeScreen = ({ navigation }) => {
 
   const onPress = ({ type }) => {
     if (type === 'gre') {
-      navigation.navigate('Study', { type: 'gre'});
+      navigation.navigate('Study', { type: 'gre' });
     }
   };
 
   return (
     <Flex flex={1} justifyContent='center'>
       <VStack space={4} alignItems='center'>
-        <HomeBox text='GRE 1500' bg='indigo.300' onPress={() => onPress({ type : 'gre'})} />
-        <HomeBox text='Collected Words' bg='indigo.600' onPress={() => onPress({ type : 'collected'})} />
+        <HomeBox text='GRE 1500' bg='indigo.300' onPress={() => onPress({ type: 'gre' })} />
+        <HomeBox text='Collected Words' bg='indigo.600' onPress={() => onPress({ type: 'collected' })} />
       </VStack>
     </Flex>
   );
