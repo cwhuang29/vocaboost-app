78f08a38f862a038ba208ece6b4983aef086c5d2
cwHuang <49531255+cwhuang29@users.noreply.github.com>
2023-05-13 19:09:04 -0700

    Feature/manage build config (#61)
    * Fix android bottom tab diff color error
    
    * Set up variants for different builds

--- app.config.js
+++ app.config.js
@@ -1,5 +1,21 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+
+const IS_PREV = process.env.APP_VARIANT === 'preview';
+
 module.exports = ({ config }) => ({
   ...config,
-  ios: { ...config.ios, googleServicesFile: process.env.GOOGLE_SERVICES_IOS }, // Should match the eas secret key
-  android: { ...config.android, googleServicesFile: process.env.GOOGLE_SERVICES_ANDROID },
+  ...(IS_DEV && { name: 'VocaBoost (dev)' }),
+  ...(IS_PREV && { name: 'VocaBoost (prev)' }),
+  ios: {
+    ...config.ios,
+    ...(IS_DEV && { bundleIdentifier: 'dev.vocaboost.com' }),
+    ...(IS_PREV && { bundleIdentifier: 'prev.vocaboost.com' }),
+    googleServicesFile: process.env.GOOGLE_SERVICES_IOS, // Should match the eas secret key
+  },
+  android: {
+    ...config.android,
+    // Error: Execution failed for task ':app:processReleaseGoogleServices'. No matching client found for package name 'dev.vocaboost.com'
+    // ...(IS_DEV && { package: 'dev.vocaboost.com' }),
+    googleServicesFile: process.env.GOOGLE_SERVICES_ANDROID,
+  },
 });
